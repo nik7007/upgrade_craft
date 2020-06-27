437dff0d27470eaad09f2cf8ac73594944295206
Nik <niky006@gmail.com>
2020-06-27 11:41:38 +0200

    Added base class for lang DataGenerators

--- src/main/java/com/nik7/upgradecraft/datagenerators/LanguageProviderUsUpgC.java
+++ src/main/java/com/nik7/upgradecraft/datagenerators/LanguageProviderUsUpgC.java
@@ -2,15 +2,12 @@ package com.nik7.upgradecraft.datagenerators;
 
 import com.nik7.upgradecraft.init.RegisterBlocks;
 import net.minecraft.data.DataGenerator;
-import net.minecraftforge.common.data.LanguageProvider;
 
 import java.util.Locale;
 
-import static com.nik7.upgradecraft.UpgradeCraft.MOD_ID;
-
-public class LanguageProviderUsUpgC extends LanguageProvider {
+public class LanguageProviderUsUpgC extends LanguageProviderUpgC {
     public LanguageProviderUsUpgC(DataGenerator gen) {
-        super(gen, MOD_ID, Locale.US.toString().toLowerCase());
+        super(gen, Locale.US);
     }
 
     @Override
