b0fd7734790a34e502dce54f8e70c96272ef8b28
Nik <niky006@gmail.com>
2020-07-05 18:40:42 +0200

    Different tank type has different model and texture

--- src/main/java/com/nik7/upgradecraft/datagenerators/BlockStateProviderUpgC.java
+++ src/main/java/com/nik7/upgradecraft/datagenerators/BlockStateProviderUpgC.java
@@ -1,12 +1,17 @@
 package com.nik7.upgradecraft.datagenerators;
 
+import com.nik7.upgradecraft.blocks.WoodenFluidTankBlock;
 import com.nik7.upgradecraft.init.RegisterBlocks;
+import com.nik7.upgradecraft.state.properties.TankType;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
+import net.minecraftforge.client.model.generators.ModelFile;
 
 import static com.nik7.upgradecraft.UpgradeCraft.MOD_ID;
+import static com.nik7.upgradecraft.blocks.WoodenFluidTankBlock.TYPE;
 
 public class BlockStateProviderUpgC extends BlockStateProvider {
     public BlockStateProviderUpgC(DataGenerator gen, ExistingFileHelper exFileHelper) {
@@ -18,8 +23,26 @@ public class BlockStateProviderUpgC extends BlockStateProvider {
         simpleBlock(RegisterBlocks.SLIMY_PLANKS_BLOCK.get(),
                 models().cubeAll("slimy_planks_block", new ResourceLocation(MOD_ID, "block/slimy_planks_block")));
 
-        simpleBlock(RegisterBlocks.WOODEN_FLUID_TANK_BLOCK.get(),
-                models().withExistingParent("wooden_fluid_tank", modLoc("block/fluid_tank"))
-                        .texture("all", modLoc("block/wooden_fluid_tank")));
+        getVariantBuilder(RegisterBlocks.WOODEN_FLUID_TANK_BLOCK.get()).forAllStatesExcept(state -> {
+            TankType tankType = state.get(TYPE);
+            ModelFile model = null;
+            switch (tankType) {
+                case SINGLE:
+                    model = models().withExistingParent("wooden_fluid_tank", modLoc("block/fluid_tank"))
+                            .texture("all", modLoc("block/wooden_fluid_tank"));
+                    break;
+                case TOP:
+                    model = models().withExistingParent("wooden_double_top_fluid_tank", modLoc("block/double_fluid_tank_up"))
+                            .texture("all", modLoc("block/double_wooden_fluid_tank"));
+                    break;
+                case BOTTOM:
+                    model = models().withExistingParent("wooden_double_bottom_fluid_tank", modLoc("block/double_fluid_tank_down"))
+                            .texture("all", modLoc("block/double_wooden_fluid_tank"));
+                    break;
+            }
+            return ConfiguredModel.builder()
+                    .modelFile(model)
+                    .build();
+        }, WoodenFluidTankBlock.WATERLOGGED);
     }
 }
