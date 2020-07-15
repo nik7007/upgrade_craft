78ef12058ebe76538d675207c07250579edd7942
Nik <niky006@gmail.com>
2020-07-15 19:49:28 +0200

    Added double tank glasses models and texture

--- src/main/java/com/nik7/upgradecraft/datagenerators/BlockStateProviderUpgC.java
+++ src/main/java/com/nik7/upgradecraft/datagenerators/BlockStateProviderUpgC.java
@@ -3,12 +3,14 @@ package com.nik7.upgradecraft.datagenerators;
 import com.nik7.upgradecraft.blocks.WoodenFluidTankBlock;
 import com.nik7.upgradecraft.init.RegisterBlocks;
 import com.nik7.upgradecraft.state.properties.TankType;
+import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
 import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.fml.RegistryObject;
 
 import static com.nik7.upgradecraft.UpgradeCraft.MOD_ID;
 import static com.nik7.upgradecraft.blocks.WoodenFluidTankBlock.TYPE;
@@ -23,30 +25,61 @@ public class BlockStateProviderUpgC extends BlockStateProvider {
         simpleBlock(RegisterBlocks.SLIMY_PLANKS_BLOCK.get(),
                 models().cubeAll("slimy_planks_block", new ResourceLocation(MOD_ID, "block/slimy_planks_block")));
 
-        getVariantBuilder(RegisterBlocks.WOODEN_FLUID_TANK_BLOCK.get()).forAllStatesExcept(state -> {
+        createTankModel(RegisterBlocks.WOODEN_FLUID_TANK_BLOCK.get(),
+                "wooden_fluid_tank",
+                "double_wooden_fluid_tank");
+
+        createTankModel(RegisterBlocks.WOODEN_FLUID_TANK_GLASSED_BLOCK.get(),
+                "wooden_fluid_tank_glassed",
+                "double_wooden_fluid_tank_glassed_up",
+                "double_wooden_fluid_tank_glassed_down"
+        );
+
+    }
+
+    private void createTankModel(Block block,
+                                 String singleTexture,
+                                 String doubleTexture) {
+        createTankModel(block, singleTexture, doubleTexture, doubleTexture);
+    }
+
+    private void createTankModel(Block block,
+                                 String singleTexture,
+                                 String doubleTopTexture,
+                                 String doubleBottomTexture) {
+
+        String topName;
+        String bottomName;
+
+        if (doubleTopTexture.equalsIgnoreCase(doubleBottomTexture)) {
+            topName = doubleTopTexture + "_top";
+            bottomName = doubleBottomTexture + "_bottom";
+        } else {
+            topName = doubleTopTexture;
+            bottomName = doubleBottomTexture;
+        }
+
+        getVariantBuilder(block).forAllStatesExcept(state -> {
             TankType tankType = state.get(TYPE);
             ModelFile model = null;
             switch (tankType) {
                 case SINGLE:
-                    model = models().withExistingParent("wooden_fluid_tank", modLoc("block/fluid_tank"))
-                            .texture("all", modLoc("block/wooden_fluid_tank"));
+                    model = models().withExistingParent(singleTexture, modLoc("block/fluid_tank"))
+                            .texture("all", modLoc("block/" + singleTexture));
                     break;
                 case TOP:
-                    model = models().withExistingParent("wooden_double_top_fluid_tank", modLoc("block/double_fluid_tank_up"))
-                            .texture("all", modLoc("block/double_wooden_fluid_tank"));
+                    model = models().withExistingParent(topName, modLoc("block/double_fluid_tank_up"))
+                            .texture("all", modLoc("block/" + doubleTopTexture));
                     break;
                 case BOTTOM:
-                    model = models().withExistingParent("wooden_double_bottom_fluid_tank", modLoc("block/double_fluid_tank_down"))
-                            .texture("all", modLoc("block/double_wooden_fluid_tank"));
+                    model = models().withExistingParent(bottomName, modLoc("block/double_fluid_tank_down"))
+                            .texture("all", modLoc("block/" + doubleBottomTexture));
                     break;
             }
             return ConfiguredModel.builder()
                     .modelFile(model)
                     .build();
         }, WoodenFluidTankBlock.WATERLOGGED);
-
-        simpleBlock(RegisterBlocks.WOODEN_FLUID_TANK_GLASSED_BLOCK.get(),
-                models().withExistingParent("wooden_fluid_tank_glassed", modLoc("block/fluid_tank"))
-                        .texture("all", modLoc("block/wooden_fluid_tank_glassed")));
     }
+
 }
