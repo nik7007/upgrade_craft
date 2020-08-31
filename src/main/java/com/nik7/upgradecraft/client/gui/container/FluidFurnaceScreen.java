69b64090bd7ec691f763d15bc9f73323ed2d6b57
Nik <niky006@gmail.com>
2020-08-31 15:44:59 +0200

    Changed how fluid tank is render in gui

--- src/main/java/com/nik7/upgradecraft/client/gui/container/FluidFurnaceScreen.java
+++ src/main/java/com/nik7/upgradecraft/client/gui/container/FluidFurnaceScreen.java
@@ -23,9 +23,19 @@ import static net.minecraft.util.ColorHelper.PackedColor.*;
 
 public class FluidFurnaceScreen extends ContainerScreen<FluidFurnaceContainer> {
     private final static ResourceLocation GUI = new ResourceLocation(MOD_ID, "textures/gui/container/fluid_furnace_gui.png");
+    private final RenderFluidTankSlot fluidTankSlot;
 
     public FluidFurnaceScreen(FluidFurnaceContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) {
         super(screenContainer, inv, titleIn);
+        FluidFurnaceTileEntity tileEntity = screenContainer.getTileEntity();
+        int capacity = tileEntity != null ? tileEntity.getCapacity() : 0;
+
+        fluidTankSlot = new RenderFluidTankSlot(
+                () -> {
+                    FluidFurnaceTileEntity te = screenContainer.getTileEntity();
+                    return te != null ? te.getFluid() : FluidStack.EMPTY;
+                },
+                capacity, 15, 16, 58, 32);
     }
 
     @Override
@@ -41,22 +51,19 @@ public class FluidFurnaceScreen extends ContainerScreen<FluidFurnaceContainer> {
         this.renderBackground(matrixStack);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
         this.renderHoveredTooltip(matrixStack, mouseX, mouseY);
+        this.fluidTooltip(matrixStack, fluidTankSlot, mouseX, mouseY);
     }
 
     @Override
     protected void drawGuiContainerForegroundLayer(MatrixStack matrixStack, int x, int y) {
         super.drawGuiContainerForegroundLayer(matrixStack, x, y);
-        FluidFurnaceTileEntity tileEntity = container.getTileEntity();
-        if (tileEntity != null) {
-            FluidStack fluid = tileEntity.getFluid();
-            int capacity = tileEntity.getCapacity();
-            renderFluid(matrixStack, fluid, capacity, x, y);
-        }
+        renderFluid(matrixStack, fluidTankSlot);
     }
 
-    private void renderFluid(MatrixStack matrixStack, FluidStack fluidStack, int capacity, int x, int y) {
-        float scale = fluidStack.getAmount() / (float) capacity;
-        int argb = RenderHelper.getColorARGB(fluidStack, scale);
+    private void renderFluid(MatrixStack matrixStack, RenderFluidTankSlot fluidTankSlot) {
+        FluidStack fluidStack = fluidTankSlot.getFluidStack();
+        float scale = fluidTankSlot.getFluidScale();
+        int argb = RenderHelper.getColorARGB(fluidTankSlot.getFluidStack(), scale);
 
         if (fluidStack.getFluid().getAttributes().isGaseous(fluidStack)) {
             scale = 1;
@@ -68,28 +75,27 @@ public class FluidFurnaceScreen extends ContainerScreen<FluidFurnaceContainer> {
         final float blue = getBlue(argb) / 255f;
         final float alpha = getAlpha(argb) / 255f;
 
-        final int baseTankX = 15;
-        final int tankWidth = 16;
-        final int baseTankY = 58;
-        final int tankHeight = 32;
+        final int baseTankX = fluidTankSlot.getBaseTankX();
+        final int tankWidth = fluidTankSlot.getTankWidth();
+        final int baseTankY = fluidTankSlot.getBaseTankY();
+        final int tankHeight = fluidTankSlot.getTankHeight();
 
         this.minecraft.getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
         RenderSystem.color4f(red, green, blue, alpha);
         blit(matrixStack, baseTankX, baseTankY - (int) (tankHeight * scale), 0, tankWidth, (int) (tankHeight * scale), fluidSprite);
 
         RenderSystem.color4f(1f, 1f, 1f, 1f);
+    }
 
-        int xOffset = (this.width - this.xSize) / 2;
-        int yOffset = (this.height - this.ySize) / 2;
-
-        if (x > baseTankX + xOffset && x < baseTankX + tankWidth + xOffset && y > baseTankY - tankHeight + yOffset && y < baseTankY + yOffset) {
-
+    private void fluidTooltip(MatrixStack matrixStack, RenderFluidTankSlot fluidTankSlot, int mouseX, int mouseY) {
+        if (fluidTankSlot.isHovered(mouseX, mouseY, guiLeft, guiTop)) {
+            FluidStack fluidStack = fluidTankSlot.getFluidStack();
             ITextComponent fluidName = fluidStack.getDisplayName();
             int amount = fluidStack.getAmount();
-            String fluidAmount = amount + "/" + capacity + "mB";
+            String fluidAmount = amount + "/" + fluidTankSlot.getCapacity() + "mB";
             List<ITextComponent> tooltip = Arrays.asList(fluidName, new StringTextComponent(fluidAmount));
 
-            this.func_243308_b(matrixStack, tooltip, x - xOffset, y - yOffset);
+            this.func_243308_b(matrixStack, tooltip, mouseX, mouseY);
         }
     }
 
---
+++ src/main/java/com/nik7/upgradecraft/client/gui/container/RenderFluidTankSlot.java
@@ -0,0 +1,64 @@
+package com.nik7.upgradecraft.client.gui.container;
+
+import net.minecraftforge.fluids.FluidStack;
+
+import java.util.function.Supplier;
+
+public class RenderFluidTankSlot {
+    private final Supplier<FluidStack> fluidStack;
+    private final int capacity;
+
+    private final int baseTankX;
+    private final int tankWidth;
+    private final int baseTankY;
+    private final int tankHeight;
+
+    public RenderFluidTankSlot(Supplier<FluidStack> fluidStack,
+                               int capacity,
+                               int baseTankX,
+                               int tankWidth,
+                               int baseTankY,
+                               int tankHeight) {
+        this.fluidStack = fluidStack;
+        this.capacity = capacity;
+        this.baseTankX = baseTankX;
+        this.tankWidth = tankWidth;
+        this.baseTankY = baseTankY;
+        this.tankHeight = tankHeight;
+    }
+
+    public FluidStack getFluidStack() {
+        return fluidStack.get();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getBaseTankX() {
+        return baseTankX;
+    }
+
+    public int getTankWidth() {
+        return tankWidth;
+    }
+
+    public int getBaseTankY() {
+        return baseTankY;
+    }
+
+    public int getTankHeight() {
+        return tankHeight;
+    }
+
+    public boolean isHovered(int mouseX, int mouseY, int guiLeft, int guiTop) {
+        mouseX -= guiLeft;
+        mouseY -= guiTop;
+        return mouseX >= baseTankX && mouseX < baseTankX + tankWidth && mouseY >= baseTankY - tankHeight && mouseY < baseTankY;
+    }
+
+    public float getFluidScale() {
+        return fluidStack.get().getAmount() / (float) capacity;
+    }
+
+}
