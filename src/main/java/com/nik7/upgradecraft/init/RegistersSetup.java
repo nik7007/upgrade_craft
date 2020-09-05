d0f0507d8b860bc34b3e945bd639a0876e091d34
Nik <niky006@gmail.com>
2020-09-05 19:13:42 +0200

    Start working on register

---
+++ src/main/java/com/nik7/upgradecraft/init/RegistersSetup.java
@@ -0,0 +1,13 @@
+package com.nik7.upgradecraft.init;
+
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryBuilder;
+
+public class RegistersSetup {
+
+    public static void init(RegistryEvent.NewRegistry event) {
+        // event
+    }
+}
