f8e0d905c924df06e68d34d9c53d44ff515f89d3
Nik <niky006@gmail.com>
2020-10-18 19:00:21 +0200

    Render fluids inside fluid infuser block

--- src/main/java/com/nik7/upgradecraft/init/ClientSetup.java
+++ src/main/java/com/nik7/upgradecraft/init/ClientSetup.java
@@ -3,6 +3,7 @@ package com.nik7.upgradecraft.init;
 import com.nik7.upgradecraft.client.gui.container.FluidFurnaceScreen;
 import com.nik7.upgradecraft.client.gui.container.FluidInfuserScreen;
 import com.nik7.upgradecraft.client.renderer.tileentity.FluidFurnaceRenderer;
+import com.nik7.upgradecraft.client.renderer.tileentity.FluidInfuserRender;
 import com.nik7.upgradecraft.client.renderer.tileentity.FluidTankRenderer;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
@@ -21,6 +22,7 @@ public class ClientSetup {
 
         FluidTankRenderer.register();
         FluidFurnaceRenderer.register();
+        FluidInfuserRender.register();
 
         ScreenManager.registerFactory(FLUID_FURNACE_CONTAINER_TYPE.get(), FluidFurnaceScreen::new);
         ScreenManager.registerFactory(FLUID_INFUSER_CONTAINER_TYPE.get(), FluidInfuserScreen::new);
