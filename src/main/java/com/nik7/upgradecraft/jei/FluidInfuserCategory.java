a50c178a0f95086907ac739282c53c7f1bf21533
Nik <niky006@gmail.com>
2020-10-24 22:33:17 +0200

    Created fluid infuser jei category

--- src/main/java/com/nik7/upgradecraft/jei/FluidInfuserCategory.java
+++ src/main/java/com/nik7/upgradecraft/jei/FluidInfuserCategory.java
@@ -1,44 +1,77 @@
 package com.nik7.upgradecraft.jei;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.nik7.upgradecraft.client.gui.container.FluidInfuserScreen;
+import com.nik7.upgradecraft.init.Config;
+import com.nik7.upgradecraft.init.RegisterBlocks;
 import com.nik7.upgradecraft.recipes.FluidInfuserRecipe;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated.StartDirection;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
+import mezz.jei.api.gui.ingredient.IGuiFluidStackGroup;
 import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidAttributes;
+
+import javax.annotation.Nonnull;
 
 public class FluidInfuserCategory implements IRecipeCategory<FluidInfuserRecipe> {
+    private static final ResourceLocation GUI = FluidInfuserScreen.GUI;
+
+    private final IDrawableStatic background;
+    private final IDrawable icon;
+    private final IDrawable bubbles;
+    private final IDrawable arrow;
 
     public FluidInfuserCategory(IGuiHelper guiHelper) {
+        this.background = guiHelper.createDrawable(GUI, 10, 16, 155, 54);
+        this.icon = guiHelper.createDrawableIngredient(new ItemStack(RegisterBlocks.FLUID_INFUSER_BLOCK.get()));
+        this.bubbles = guiHelper.drawableBuilder(GUI, 176, 0, 15, 14).buildAnimated(300, StartDirection.TOP, true);
+        this.arrow = guiHelper.drawableBuilder(GUI, 176, 14, 24, 17).buildAnimated(200, StartDirection.LEFT, false);
+
     }
 
     @Override
+    public void draw(@Nonnull FluidInfuserRecipe recipe, @Nonnull MatrixStack matrixStack, double mouseX, double mouseY) {
+        bubbles.draw(matrixStack, 42, 23);
+        arrow.draw(matrixStack, 94, 21);
+    }
+
+    @Override
+    @Nonnull
     public ResourceLocation getUid() {
         return UpgCRecipeCategoryUid.FLUID_INFUSER;
     }
 
     @Override
+    @Nonnull
     public Class<? extends FluidInfuserRecipe> getRecipeClass() {
         return FluidInfuserRecipe.class;
     }
 
     @Override
+    @Nonnull
     public String getTitle() {
         return I18n.format("block.upgradecraft.fluid_infuser_block");
     }
 
     @Override
+    @Nonnull
     public IDrawable getBackground() {
-        return null;
+        return background;
     }
 
     @Override
+    @Nonnull
     public IDrawable getIcon() {
-        return null;
+        return icon;
     }
 
     @Override
@@ -49,11 +82,16 @@ public class FluidInfuserCategory implements IRecipeCategory<FluidInfuserRecipe>
     }
 
     @Override
-    public void setRecipe(IRecipeLayout iRecipeLayout, FluidInfuserRecipe fluidInfuserRecipe, IIngredients iIngredients) {
+    public void setRecipe(IRecipeLayout iRecipeLayout, @Nonnull FluidInfuserRecipe fluidInfuserRecipe, @Nonnull IIngredients iIngredients) {
         IGuiItemStackGroup guiItemStacks = iRecipeLayout.getItemStacks();
-        iRecipeLayout.getFluidStacks().init(0, true, 1, 0);
-        guiItemStacks.init(0, true, 8, 0);
-        guiItemStacks.init(1, true, 16, 0);
-        guiItemStacks.init(2, false, 61, 18);
+        guiItemStacks.init(0, true, 41, 3);
+        guiItemStacks.init(1, true, 71, 3);
+        guiItemStacks.init(2, false, 131, 21);
+
+        IGuiFluidStackGroup guiFluidStacks = iRecipeLayout.getFluidStacks();
+        guiFluidStacks.init(3, true, 5, 10, 16, 32, Config.TANK_CAPACITY.get() * FluidAttributes.BUCKET_VOLUME, true, null);
+
+        guiItemStacks.set(iIngredients);
+        guiFluidStacks.set(iIngredients);
     }
 }
--- src/main/java/com/nik7/upgradecraft/jei/UpgCPlugin.java
+++ src/main/java/com/nik7/upgradecraft/jei/UpgCPlugin.java
@@ -1,58 +1,64 @@
 package com.nik7.upgradecraft.jei;
 
 import com.nik7.upgradecraft.client.gui.container.FluidFurnaceScreen;
+import com.nik7.upgradecraft.client.gui.container.FluidInfuserScreen;
 import com.nik7.upgradecraft.container.FluidFurnaceContainer;
+import com.nik7.upgradecraft.container.FluidInfuserContainer;
 import com.nik7.upgradecraft.init.RegisterBlocks;
 import com.nik7.upgradecraft.recipes.FluidInfuserRecipeManager;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.helpers.IJeiHelpers;
 import mezz.jei.api.registration.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
+import javax.annotation.Nonnull;
+
 import static com.nik7.upgradecraft.UpgradeCraft.MOD_ID;
 
+@SuppressWarnings("unused")
 @JeiPlugin
 public class UpgCPlugin implements IModPlugin {
 
-    // private FluidFurnaceCategory fluidFurnaceCategory;
-
     @Override
+    @Nonnull
     public ResourceLocation getPluginUid() {
         return new ResourceLocation(MOD_ID, "jei_plugin");
     }
 
     @Override
     public void registerCategories(IRecipeCategoryRegistration registration) {
-        //   IJeiHelpers jeiHelpers = registration.getJeiHelpers();
-        //   IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
-        //   this.fluidFurnaceCategory = new FluidFurnaceCategory(guiHelper);
-        //   registration.addRecipeCategories(this.fluidFurnaceCategory);
+        IJeiHelpers jeiHelpers = registration.getJeiHelpers();
+        IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
+        FluidInfuserCategory fluidInfuserCategory = new FluidInfuserCategory(guiHelper);
+        registration.addRecipeCategories(fluidInfuserCategory);
     }
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
         assert Minecraft.getInstance().world != null;
         registration.addRecipes(FluidInfuserRecipeManager.getManager(Minecraft.getInstance().world).getRecipes(), UpgCRecipeCategoryUid.FLUID_INFUSER);
-        // RecipeManager recipeManager = Minecraft.getInstance().world.getRecipeManager();
-        // List<FurnaceRecipe> furnaceRecipes = recipeManager.getRecipesForType(IRecipeType.SMELTING);
-        // registration.addRecipes(furnaceRecipes, UpgCRecipeCategoryUid.FLUID_FURNACE);
     }
 
     @Override
     public void registerGuiHandlers(IGuiHandlerRegistration registration) {
         registration.addRecipeClickArea(FluidFurnaceScreen.class, 78, 32, 28, 23, VanillaRecipeCategoryUid.FURNACE);
+        registration.addRecipeClickArea(FluidInfuserScreen.class, 105, 37, 25, 18, UpgCRecipeCategoryUid.FLUID_INFUSER);
     }
 
     @Override
     public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
         registration.addRecipeTransferHandler(FluidFurnaceContainer.class, VanillaRecipeCategoryUid.FURNACE, 0, 1, 2, 36);
+        registration.addRecipeTransferHandler(FluidInfuserContainer.class, UpgCRecipeCategoryUid.FLUID_INFUSER, 0, 2, 3, 36);
     }
 
     @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
         registration.addRecipeCatalyst(new ItemStack(RegisterBlocks.FLUID_FURNACE_BLOCK.get()), VanillaRecipeCategoryUid.FURNACE);
+        registration.addRecipeCatalyst(new ItemStack(RegisterBlocks.FLUID_INFUSER_BLOCK.get()), UpgCRecipeCategoryUid.FLUID_INFUSER);
     }
 }
