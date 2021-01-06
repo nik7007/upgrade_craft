2dc4a6bf2f21b3cc89c4caa566db0ec904a1a592
Nik <niky006@gmail.com>
2021-01-07 00:58:23 +0100

    Changed item group icon

--- src/main/java/com/nik7/upgradecraft/init/ItemGroupUpgC.java
+++ src/main/java/com/nik7/upgradecraft/init/ItemGroupUpgC.java
@@ -3,14 +3,14 @@ package com.nik7.upgradecraft.init;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 
-import static com.nik7.upgradecraft.init.RegisterBlocks.SLIMY_PLANKS_BLOCK;
+import static com.nik7.upgradecraft.init.RegisterBlocks.WOODEN_FLUID_TANK_GLASSED_BLOCK;
 
 public class ItemGroupUpgC {
 
     public static final ItemGroup ITEM_GROUP = new ItemGroup("upgrade_craft") {
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(SLIMY_PLANKS_BLOCK.get());
+            return new ItemStack(WOODEN_FLUID_TANK_GLASSED_BLOCK.get());
         }
     };
 }
