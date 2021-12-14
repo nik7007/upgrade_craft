769732413f47b9a8a62a261f449a6a935cc173bc
Nik <niky006@gmail.com>
2021-12-14 13:43:29 +0100

    feat: change item tab icon

--- src/main/java/com/nik7/upgradecraft/init/CreativeModeTabUpgC.java
+++ src/main/java/com/nik7/upgradecraft/init/CreativeModeTabUpgC.java
@@ -4,14 +4,14 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import static com.nik7.upgradecraft.init.RegisterBlocks.SLIMY_PLANKS_BLOCK;
+import static com.nik7.upgradecraft.init.RegisterBlocks.WOODEN_FLUID_TANK_GLASSED_BLOCK;
 
 
 public class CreativeModeTabUpgC {
     public static final CreativeModeTab ITEM_GROUP = new CreativeModeTab("upgrade_craft") {
         @Override
         public @NotNull ItemStack makeIcon() {
-            return new ItemStack(SLIMY_PLANKS_BLOCK.get());
+            return new ItemStack(WOODEN_FLUID_TANK_GLASSED_BLOCK.get());
         }
 
     };
