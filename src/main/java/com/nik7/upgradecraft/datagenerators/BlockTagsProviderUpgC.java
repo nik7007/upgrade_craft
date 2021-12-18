a77794df2def7f4a179ab06d1c239043938cefac
Nik <niky006@gmail.com>
2021-12-18 23:55:32 +0100

    feat: added blocks to mineable tag

--- src/main/java/com/nik7/upgradecraft/datagenerators/BlockTagsProviderUpgC.java
+++ src/main/java/com/nik7/upgradecraft/datagenerators/BlockTagsProviderUpgC.java
@@ -3,11 +3,11 @@ package com.nik7.upgradecraft.datagenerators;
 import com.nik7.upgradecraft.crafting.TagUpgC;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 import static com.nik7.upgradecraft.UpgradeCraft.MOD_ID;
-import static com.nik7.upgradecraft.init.RegisterBlocks.WOODEN_FLUID_TANK_BLOCK;
-import static com.nik7.upgradecraft.init.RegisterBlocks.WOODEN_FLUID_TANK_GLASSED_BLOCK;
+import static com.nik7.upgradecraft.init.RegisterBlocks.*;
 
 public class BlockTagsProviderUpgC extends BlockTagsProvider {
 
@@ -17,12 +17,26 @@ public class BlockTagsProviderUpgC extends BlockTagsProvider {
 
     @Override
     protected void addTags() {
-        tag(TagUpgC.Blocks.WOODEN_TANK)
-                .add(WOODEN_FLUID_TANK_BLOCK.get(),
-                        WOODEN_FLUID_TANK_GLASSED_BLOCK.get());
+        tag(BlockTags.MINEABLE_WITH_AXE)
+                .add(
+                        SLIMY_PLANKS_BLOCK.get(),
+                        WOODEN_FLUID_TANK_BLOCK.get(),
+                        WOODEN_FLUID_TANK_GLASSED_BLOCK.get()
+                );
 
-      //  tag(BlockTags.WALLS)
-      //          .add(CLAY_BRICKS_WALL_BLOCK.get());
+        tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(
+                        FUNNEL_BLOCK.get()
+                );
+
+        tag(TagUpgC.Blocks.WOODEN_TANK)
+                .add(
+                        WOODEN_FLUID_TANK_BLOCK.get(),
+                        WOODEN_FLUID_TANK_GLASSED_BLOCK.get()
+                );
+
+        //  tag(BlockTags.WALLS)
+        //          .add(CLAY_BRICKS_WALL_BLOCK.get());
     }
 
     @Override
