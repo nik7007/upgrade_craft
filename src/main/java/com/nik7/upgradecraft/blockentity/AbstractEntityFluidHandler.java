c06426e08226fca9c452ce6e96ee3bce822aea23
Nik <niky006@gmail.com>
2021-12-26 17:33:18 +0100

    style: added missing brackets

--- src/main/java/com/nik7/upgradecraft/blockentity/AbstractEntityFluidHandler.java
+++ src/main/java/com/nik7/upgradecraft/blockentity/AbstractEntityFluidHandler.java
@@ -74,8 +74,9 @@ public abstract class AbstractEntityFluidHandler extends BlockEntity {
     @Override
     @Nonnull
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction facing) {
-        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY)
+        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
             return holder.cast();
+        }
         return super.getCapability(capability, facing);
     }
 
